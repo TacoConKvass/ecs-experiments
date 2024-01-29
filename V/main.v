313eb2cbaae45e094909d1490759f7bf7ca68101
Eryk Fruń <efrun@mickiewicz103.lan>
2024-01-29 10:17:00 +0100

    [ V ] - Update demo

--- V/main.v
+++ V/main.v
@@ -9,11 +9,11 @@ import math
 import benchmark
 
 const color := [gx.red, gx.blue, gx.yellow, gx.green]
-const window_width = 900
-const window_height = 600
-const entity_count = 2000
-const entity_size = 3
-const draw_per_call = 100
+const window_width = 1280
+const window_height = 720
+const entity_count = 16000
+const entity_size = 2
+const draw_per_call = 1000
 
 struct App {
 	mut:
@@ -55,11 +55,11 @@ fn main() {
 				position_c.data[owner] = position_c.data[owner].add(velocity_c.data[index])
 				
 				if (position_c.data[owner].x > window_width && velocity_c.data[index].x > 0) || (position_c.data[owner].x < 0 && velocity_c.data[index].x < 0) {
-					velocity_c.data[index] = Vec2{-velocity_c.data[index].x, velocity_c.data[index].y}.rotated_by(math.radians(rand.f64_in_range(-20, 20) or { 0 }))
+					velocity_c.data[index] = Vec2{-velocity_c.data[index].x, velocity_c.data[index].y}.rotated_by(math.radians(rand.f64_in_range(-10, 10) or { 0 }))
 				}
 
 				if (position_c.data[owner].y > window_height && velocity_c.data[index].y > 0) || (position_c.data[owner].y < 0 && velocity_c.data[index].y < 0) {
-					velocity_c.data[index] = Vec2{velocity_c.data[index].x, -velocity_c.data[index].y}.rotated_by(math.radians(rand.f64_in_range(-20, 20) or { 0 }))
+					velocity_c.data[index] = Vec2{velocity_c.data[index].x, -velocity_c.data[index].y}.rotated_by(math.radians(rand.f64_in_range(-10, 10) or { 0 }))
 				}
 
 				/*
@@ -87,24 +87,18 @@ fn frame(mut app App) {
 	app.gg.begin()
 	app.gg.end()
 
-	cells := [][]Vec2 {len: 4, cap : 4, init: pos_c.data.filter(it.x > (window_width / 2) * index%2 && it.x <= (window_width / 2) * (index%2 + 1) &&
-		it.y > (window_height / 2) * math.floor(index/2) && it.y <= (window_height / 2) * math.floor((index+1)/2))}
-	//println(cells)
-
-	for box in 0 .. cells.len {
-		for rep in 0 .. cells[box].len / draw_per_call {
-			app.gg.begin()
+	for rep in 0 .. pos_c.data.len / draw_per_call {
+		app.gg.begin()
 			
-			mut temp := Vec2{}
-			for entity in cells[box][rep*draw_per_call .. (rep + 1)*draw_per_call] {
-				if entity.distance_to(temp) > 2 {
-					app.gg.draw_circle_filled(f32(entity.x), f32(entity.y), entity_size, color[box])
-				}
-				temp = entity
+		mut temp := Vec2{}
+		for entity in pos_c.data[rep*draw_per_call .. (rep + 1)*draw_per_call] {
+			if entity.distance_to(temp) > 2 {
+				app.gg.draw_circle_filled(f32(entity.x), f32(entity.y), entity_size, color[rep%4])
 			}
-
-			app.gg.end(how: .passthru)
+			temp = entity
 		}
+
+		app.gg.end(how: .passthru)
 	} 
 
 	app.gg.begin()
