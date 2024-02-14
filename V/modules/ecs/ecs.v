db083926aa6ef9c049eb431df80d4a6c60d8c7b9
Eryk Fruń <efrun@mickiewicz103.lan>
2024-02-14 13:06:08 +0100

    [ Remove ] - unused main function in ecs module

--- V/modules/ecs/ecs.v
+++ V/modules/ecs/ecs.v
@@ -77,58 +77,6 @@ pub fn (mut w World) register_system(name string) {
 
 }
 
-fn main_2() !{
-
-	entity_count := 100
-	println("All entities: ${entity_count}; Moving entities: ${entity_count/2}")
-
-	mut wld := &World{}
-
-	/* ==== Add all entities to world ==== */
-	wld.entities << []int{len: entity_count, init: index+1}	
-
-	/* ==== Attach components to entities ==== */
-	mut p_data := ComponentData[Vec2]{"position", 
-		[ Vec2{3, 1} ].repeat(wld.entities.len),
-		wld.entities
-	}
-	wld.components.c_vec << p_data
-
-	mut v_data := ComponentData[Vec2]{"velocity", 
-		[ Vec2{1, 0}, Vec2{0, 1} ].repeat(wld.entities.len/4),
-		wld.entities.filter(it % 2 == 0)
-	}
-	wld.components.c_vec << v_data
-
-	/* ==== Add systems ==== */
-	wld.systems = {
-		...wld.systems
-		"move": fn (mut wld &World) {
-			mut position_c := wld.components.c_vec[0]
-			velocity_c := wld.components.c_vec[1]
-
-			for index in 0 .. velocity_c.owner.len {
-				owner := velocity_c.owner[index]
-				position_c.data[owner - 1] = position_c.data[owner - 1].add(velocity_c.data[index])
-			}
-		}
-	}
-
-	/* ==== Tests ==== */
-	println("==== ComponentList ====")
-	move_test(mut wld, 1)
-	move_test(mut wld, 10)
-	move_test(mut wld, 100)
-	move_test(mut wld, 1_000)
-	move_test(mut wld, 10_000)
-	move_test(mut wld, 100_000)
-	move_test(mut wld, 1_000_000)
-	println("\n")
-
-	println(wld.get_component[Vec2]("position")!)
-	println(wld.get_component[Vec2]("velocity")!.owner)
-}
-
 fn move_test(mut wld World, repetition int) {
 	mut b := benchmark.start()
 	for rep in 0 .. repetition {
