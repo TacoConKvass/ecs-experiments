c62d834ac7bc7c8a8a4e06a21c98e021e1556ece
TacoConKvass <e_frun@o2.pl>
2024-02-28 21:45:07 +0100

    update

--- V/main.v
+++ V/main.v
@@ -7,13 +7,12 @@ import ecs
 import rand
 import math
 
-import benchmark
-
 const color := [gx.red, gx.blue, gx.yellow, gx.green]
 const window_width = 1280
 const window_height = 720
-const entity_count = 2000
-const draw_per_call = 100
+const entity_count = 2
+const draw_per_call = 1
+const entity_size = 40
 
 struct App {
 	mut:
@@ -53,11 +52,11 @@ fn main() {
 				position_c.data[owner] = position_c.data[owner].add(velocity_c.data[index])
 				
 				if (position_c.data[owner].x > window_width && velocity_c.data[index].x > 0) || (position_c.data[owner].x < 0 && velocity_c.data[index].x < 0) {
-					velocity_c.data[index] = Vec2{-velocity_c.data[index].x, velocity_c.data[index].y}.rotated_by(math.radians(rand.f64_in_range(-10, 10) or { 0 }))
+					velocity_c.data[index] = Vec2{-velocity_c.data[index].x, velocity_c.data[index].y}//.rotated_by(math.radians(rand.f64_in_range(-10, 10) or { 0 }))
 				}
 
 				if (position_c.data[owner].y > window_height && velocity_c.data[index].y > 0) || (position_c.data[owner].y < 0 && velocity_c.data[index].y < 0) {
-					velocity_c.data[index] = Vec2{velocity_c.data[index].x, -velocity_c.data[index].y}.rotated_by(math.radians(rand.f64_in_range(-10, 10) or { 0 }))
+					velocity_c.data[index] = Vec2{velocity_c.data[index].x, -velocity_c.data[index].y}//.rotated_by(math.radians(rand.f64_in_range(-10, 10) or { 0 }))
 			}
 		}
 	}
@@ -80,8 +79,8 @@ fn main() {
 			return 0
 		})
 
-		for rep in 0 .. position_data_x.len / 10 {
-			mut position_data_y := position_data_x[rep * 10 .. (rep+1) * 10].clone()
+		for rep in 0 .. position_data_x.len /* / 50*/ {
+			mut position_data_y := position_data_x.clone()/*[rep * 50 .. (rep+1) * 50]*/
 			position_data_y.sort_with_compare(fn (a &Vec2, b &Vec2) int {
 				if a.y > b.y {
 					return 1
@@ -93,16 +92,18 @@ fn main() {
 				return 0
 			})
 
+			println(position_data_y)
+
 			for entity in 0 .. position_data_y.len - 1 {
-				if position_data_y[entity].distance_to(position_data_y[entity + 1]) < 8 {
+				if position_data_y[entity].distance_to(position_data_y[entity + 1]) < entity_size*2.05 {
 					owner_id := position_c.owner[position_c.data.index(position_data_y[entity])]
 					owner2_id := position_c.owner[position_c.data.index(position_data_y[entity + 1])]
 					if owner_id in velocity_c.owner {
 						owner_index := velocity_c.owner.index(owner_id)
 						owner2_index := velocity_c.owner.index(owner2_id)
-						mut alias := &velocity_c.data[owner_index]
-						mut alias2 := &velocity_c.data[owner2_index]
-						velocity_c.data[owner_index] = position_data_y[entity].substract(position_data_y[entity + 1]).scale(alias.get_dot(alias2) / math.pow(position_data_y[entity].distance_to(position_data_y[entity + 1]), 2)).normalize().scale(4)
+						old_vel1 := velocity_c.data[owner_index]
+						velocity_c.data[owner_index]  = position_data_y[entity].substract(position_data_y[entity + 1]).scale(velocity_c.data[owner_index].substract(velocity_c.data[owner2_index]).get_dot(position_data_y[entity].substract(position_data_y[entity + 1])) / math.pow(position_data_y[entity].distance_to(position_data_y[entity + 1]), 2)).normalize().scale(8)
+						velocity_c.data[owner2_index] = position_data_y[entity + 1].substract(position_data_y[entity]).scale(velocity_c.data[owner2_index].substract(old_vel1).get_dot(position_data_y[entity + 1].substract(position_data_y[entity])) / math.pow(position_data_y[entity + 1].distance_to(position_data_y[entity]), 2)).normalize().scale(8)
 					}
 				}
 			}
@@ -115,36 +116,28 @@ fn main() {
 }
 
 fn frame(mut app App) {
-	mut bg := benchmark.start()
-	mut b := benchmark.start()
 	app.world.systems["handle_movement"](mut app.world)
-	if app.gg.frame > 800 {
-		app.world.systems["handle_collision"](mut app.world)
-	}
-	b.measure("Handle movement")
+	app.world.systems["handle_collision"](mut app.world)
 	
 	mut pos_c := app.world.get_component[Vec2]("position") or { panic("Position not found") }
 
 	app.gg.begin()
 	app.gg.end()
 
-	mut b2 := benchmark.start()
 	for rep in 0 .. pos_c.data.len / draw_per_call {
 		app.gg.begin()
 			
 		mut temp := Vec2{}
 		for entity in pos_c.data[rep*draw_per_call .. (rep + 1)*draw_per_call] {
 			if entity.distance_to(temp) > 2 {
-				app.gg.draw_circle_filled(f32(entity.x), f32(entity.y), 2, color[rep%4])
+				app.gg.draw_circle_filled(f32(entity.x), f32(entity.y), entity_size, color[rep%4])
 			}
 			temp = entity
 		}
 		app.gg.end(how: .passthru)
 	} 
-	b2.measure("Draw")
 
 	app.gg.begin()
 	app.gg.show_fps()
 	app.gg.end(how: .passthru)
-	bg.measure("Frame")
 }
