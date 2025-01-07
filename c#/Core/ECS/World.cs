f4df53d5896517c0354e5e78af2a18a06a312ee2
TacoConKvass <e_frun@o2.pl>
2025-01-07 11:40:38 +0100

    [WIP][C#] Test

---
+++ c#/Core/ECS/World.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Core.ECS;
+
+public class World() {
+    private byte[] entityFlags = [];
+    private int componentCount = 0;
+    public void RegisterComponent<T>() where T : struct { 
+        Component<T>.ID = componentCount++;
+    }
+
+    public void GetComponent<T>() where T : struct { }
+}
+
+public static class Component<T> where T : struct {
+    public static int ID;
+}
+
+struct Entity(int id) {
+    public int ID = id;
+    public Entity AddComponent() { return this; }
+}
