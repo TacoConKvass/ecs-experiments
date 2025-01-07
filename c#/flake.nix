ef68334e1479acdfdadc38974c1ac719d6eed9d6
TacoConKvass <e_frun@o2.pl>
2025-01-07 13:48:40 +0100

    [WIP] Tooling

---
+++ c#/flake.nix
@@ -0,0 +1,18 @@
+{
+	description = "ECS dev tools - C#";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+	};
+
+	outputs = { self, nixpkgs }: let 
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { inherit system; };
+	in {
+		devShells.${system}.default = pkgs.mkShell {
+				packages = [
+					pkgs.dotnet-sdk
+			];
+		};
+	};
+}
