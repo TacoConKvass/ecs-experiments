7986d695c76c70adb6d9ff7759e07b5590153572
TacoConKvass <e_frun@o2.pl>
2025-01-14 14:14:14 +0100

    [WIP][C#]

--- c#/Core/ECS/Query.cs
+++ c#/Core/ECS/Query.cs
@@ -4,22 +4,78 @@ namespace Core.ECS;
 
 public static class Query<T> 
 		where T : struct {
-	public static List<T> values = [];
+	public static List<int> values = [];
 	public static int Count = 0;
 	static bool Dirty = true;
 
-	public static T[] Execute(World world) {
+	public static int[] Execute(World world) {
 		var component = world.GetComponent<T>();
 		Dirty = component.Dirty;
-		if (Dirty) {
-			values = [];
-			for (int i = 0; i < world.Entities.Length; i++) {
-				if (!world.Entities[i].Has(component.Offset)) continue;
-				
-				values.Add(world.GetComponent<T>().DataStore.Get(i));
-			}
-			Count = values.Count;
+		
+		if (!Dirty) return values.ToArray();
+
+		values = [];
+		for (int i = 0; i < world.Entities.Length; i++) {
+			if (!world.Entities[i].Has(component.Offset)) continue;		
+			values.Add(i);
 		}
+		
+		Count = values.Count;
+
+		return values.ToArray();
+	}
+}
+
+public static class Query<T1, T2>
+		where T1 : struct
+		where T2 : struct {
+	public static List<int> values = [];
+	public static int Count = 0;
+	static bool Dirty = true;
+
+	public static int[] Execute(World world) {
+		var component1 = world.GetComponent<T1>();
+		var component2 = world.GetComponent<T2>();
+		Dirty = component1.Dirty || component2.Dirty;
+
+		if (!Dirty) return values.ToArray();
+
+		values = [];
+		for (int i = 0; i < world.Entities.Length; i++) {
+			if (!world.Entities[i].Has(component1.Offset) || !world.Entities[i].Has(component2.Offset)) continue;
+			values.Add(i);
+		}
+
+		Count = values.Count;
+
+		return values.ToArray();
+	}
+}
+
+
+public static class Query<T1, T2, T3>
+		where T1 : struct
+		where T2 : struct
+		where T3 : struct {
+	public static List<int> values = [];
+	public static int Count = 0;
+	static bool Dirty = true;
+
+	public static int[] Execute(World world) {
+		var component1 = world.GetComponent<T1>();
+		var component2 = world.GetComponent<T2>();
+		var component3 = world.GetComponent<T3>();
+		Dirty = component1.Dirty || component2.Dirty;
+
+		if (!Dirty) return values.ToArray();
+
+		values = [];
+		for (int i = 0; i < world.Entities.Length; i++) {
+			if (!world.Entities[i].Has(component1.Offset) || !world.Entities[i].Has(component2.Offset) || !!world.Entities[i].Has(component3.Offset)) continue;
+			values.Add(i);
+		}
+
+		Count = values.Count;
 
 		return values.ToArray();
 	}
--- c#/Program.cs
+++ c#/Program.cs
@@ -6,36 +6,49 @@ using System.Numerics;
 World world = ECS.CreateWorld()
 	.RegisterComponent<Position>()
 	.RegisterComponent<Velocity>()
+	.RegisterComponent<Color>()
 	.Initialise();
 
 for (int i = 0; i < 1_000_000; i++) {
 	world.GetEntity(i)
-		.Set<Position>(new(Random.Shared.Next(1280), Random.Shared.Next(720)));
+		.Set<Position>(new (0, 0))
+		.Set<Velocity>(new (Random.Shared.NextSingle(), Random.Shared.NextSingle()))
+		.Set<Color>(new (Random.Shared.Next(255), Random.Shared.Next(255), Random.Shared.Next(255)));
 }
 
 Raylib.InitWindow(1280, 720, "Wah");
 Raylib.SetExitKey(KeyboardKey.Null);
 
+// Raylib.SetTargetFPS(60);
+
 ref var pos = ref world.GetComponent<Position>();
 var vel = world.GetComponent<Velocity>();
+var ren = world.GetComponent<Color>().DataStore.Data;
+
+ref var posi = ref pos.DataStore.Data;
+
+var magic = Raylib.LoadTexture("Assets/Magic.png");
+float deltaTime = 0;
 
 while (!Raylib.WindowShouldClose()) {
-	Query<Position>.Execute(world);
-	for (int i = 0; i < Query<Position>.Count; i++) {
-		pos.DataStore.Data[i].Value += Vector2.UnitX;
-	}
 
-	pos.Dirty = true;
+	Query<Position>.Execute(world);
+	foreach (var i in Query<Position>.Execute(world)) {
+		pos.DataStore.Data[i].Value += vel.DataStore.Data[i].Value * deltaTime;
+	}
 
 	Raylib.BeginDrawing();
 	Raylib.ClearBackground(Color.Black);
-	foreach (var test in Query<Position>.Execute(world))
-		Raylib.DrawLine((int)test.Value.X, (int)test.Value.Y, (int)test.Value.X + 1, (int)test.Value.Y + 1, Color.Beige);
+	
+	foreach (var i in Query<Position, Color>.Execute(world)) {
+		Raylib.DrawTexture(magic, (int)posi[i].Value.X, (int)posi[i].Value.Y, ren[i]);
+	}
+
 	Raylib.DrawFPS(20, 20);
 
 	Raylib.EndDrawing();
-
-	pos.Dirty = false;
+	deltaTime = Raylib.GetFrameTime() / 0.0166667f;
+	Console.WriteLine(deltaTime);
 }
 
 public struct Position(float x, float y) {
