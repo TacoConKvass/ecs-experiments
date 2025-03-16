83e239deb18ae810960b7b245dd017f9e7d3e0fc
TacoConKvass <e_frun@o2.pl>
2025-03-16 23:43:59 +0100

    [Fix] World.Entities fill [Add] Entity.CopyFrom

--- c#/Core/World.cs
+++ c#/Core/World.cs
@@ -2,6 +2,7 @@ using ECS.Collections;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace ECS;
@@ -13,6 +14,7 @@ public class World {
 
 		public Entity this[int index] {
 			get => new Entity(world, index);
+			set => new Entity(world, index).CopyFrom(value);
 		} 
 	}	
 
@@ -37,13 +39,14 @@ public class World {
 		};
 
 		Array.Resize(ref Entities.componentFlags, initial_size);
-		Array.Fill(Entities.componentFlags, new BitArray(ComponentCount));
+		for (int i = 0; i < initial_size; i++) Entities.componentFlags[i] = new BitArray(ComponentCount);
 	}
 
 	public void AddComponent<T>() where T : struct {
 		ComponentStorage<T>.AddTo(this);
 		components.Add(GetComponent<T>().Data);
 		ComponentCount++;
+		
 		for (int i = 0; i < Entities.componentFlags.Length; i++) {
 			Entities.componentFlags[i].Length = ComponentCount;
 		}
@@ -52,40 +55,67 @@ public class World {
 	public ComponentRecord GetComponent<T>() where T : struct => ComponentStorage<T>.GetFrom(this);
 
 	internal int GetFreeId() {
-		return freeEntityIds.TryPop(out int id) ? id : nextEntityId++;
+		int freeId = freeEntityIds.TryPop(out int id) ? id : nextEntityId++;
+		
+		if (freeId >= Entities.componentFlags.Length) {
+			int old_length = Entities.componentFlags.Length;
+			
+			Array.Resize(ref Entities.componentFlags, Math.Max(freeId, old_length * 2));
+			for (int i = old_length; i < Entities.componentFlags.Length; i++) Entities.componentFlags[i] = new BitArray(ComponentCount);
+		}
+
+		return freeId;
 	}
 }
 
 public ref struct Entity {
 	public readonly int Id;
-	public ref BitArray ComponentFlags => ref world.Entities.componentFlags[Id];
-
+	
 	internal readonly World world;
-
+	internal ref BitArray componentFlags;
+	
 	public Entity(World e_world, int? id = null) {
 		Id = id ?? e_world.GetFreeId();
 		world = e_world;
+		componentFlags = ref world.Entities.componentFlags[Id];
 	}
 
 	public bool Has<T>() where T : struct {
-		int index = ComponentStorage<T>.GetFrom(world).Id;
-		return ComponentFlags[index];
+		int index = world.GetComponent<T>().Id;
+		return componentFlags[index];
 	}
 
 	public T? Get<T>() where T : struct {
-		return (T?)(ComponentStorage<T>.GetFrom(world).Data.TryGet(Id));
+		return (T?)world.GetComponent<T>().Data.TryGet(Id);
 	}
 
 	public void Set<T>(T data) where T : struct {
-		ComponentRecord component = ComponentStorage<T>.GetFrom(world);
-		ComponentFlags[component.Id] = true;
+		ComponentRecord component = world.GetComponent<T>();
+		componentFlags[component.Id] = true;
 		component.Data.Set(Id, data);
 	}
 
 	public void Remove<T>() where T : struct {
-		ComponentRecord component = ComponentStorage<T>.GetFrom(world);
-		ComponentFlags[component.Id] = false;
+		ComponentRecord component = world.GetComponent<T>();
+		componentFlags[component.Id] = false;
 		component.Data.Delete(Id);
+	}
+
+	public void Destroy() {
+		if (componentFlags.HasAnySet()) {
+			foreach (SparseSetBase component in world.components) component.Delete(Id);
+		}
+
+		componentFlags.SetAll(false);
+		world.freeEntityIds.Push(Id);
+	}
+
+	public void CopyFrom(Entity incoming) {
+		for (int i = 0; i < world.ComponentCount; i++) {
+			if (!incoming.componentFlags[i]) continue;
+
+			world.components[i].Set(Id, incoming.world.components[i].TryGet(incoming.Id));
+		}
 	}
 }
 
--- c#/Demo/Program.cs
+++ c#/Demo/Program.cs
@@ -14,17 +14,22 @@ Assert.Success("Component with id 0 is of type Vector2", component.Id == 0);
 
 var new_entity = new ECS.Entity(world);
 new_entity.Set(Vector2.Zero);
-Assert.Success("BitArray values matches between Entity instance and world.Entities", new_entity.ComponentFlags == world.Entities.componentFlags[0]);
+Assert.Success("BitArray values matches between Entity instance and world.Entities", new_entity.componentFlags == world.Entities.componentFlags[0]);
 
 var entity_from_map = world.Entities[0];
 entity_from_map.Set(new Vector2(1, 1));
 Assert.Success("new_entity is the same as entity_from_map if they share the same id", new_entity.Get<Vector2>()! == entity_from_map.Get<Vector2>()!);
 
 var entity_from_map_1 = world.Entities[1];
-Assert.Failure("new_entity is not the same as entity_from_map_1 as they do not share the same id", new_entity.ComponentFlags == entity_from_map_1.ComponentFlags);
+Assert.Failure("new_entity is not the same as entity_from_map_1 as they do not share the same id", new_entity.componentFlags == entity_from_map_1.componentFlags);
+
+world.Entities[1] = entity_from_map;
+Assert.Success("Data of world.Entities[0] copied to world.Entities[1]", new_entity.Get<Vector2>()! == entity_from_map.Get<Vector2>()!);
 
 entity_from_map.Remove<Vector2>();
 Assert.Success("Vector2 component removed from entity_from_map", entity_from_map.Get<Vector2>() == null);
+
+
 
 Assert.FinalStatus();
 
